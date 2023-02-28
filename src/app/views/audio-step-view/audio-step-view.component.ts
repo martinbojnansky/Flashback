023ebe1200cb82a847d04f7d4b9450c18e93664f
Martin Bojnansky <martin.bojnansky@outlook.com>
2023-03-01 00:49:05 +0100

    Revert refactoring and add zip

--- src/app/views/audio-step-view/audio-step-view.component.ts
+++ src/app/views/audio-step-view/audio-step-view.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { Subject } from 'rxjs';
+import * as JSZip from 'jszip';
+import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
 import { OnsetsService } from './services/onsets.service';
 
 @Component({
@@ -13,27 +14,60 @@ import { OnsetsService } from './services/onsets.service';
   providers: [OnsetsService],
   imports: [CommonModule, RouterModule],
 })
-export class AudioStepViewComponent {
+export class AudioStepViewComponent implements OnDestroy {
+  readonly srcs$ = new BehaviorSubject<{ url: string; safeUrl: SafeUrl }[]>([]);
+
+  private readonly destroyed$ = new Subject<boolean>();
+
   constructor(
     protected onsetsService: OnsetsService,
     protected sanitizer: DomSanitizer
   ) {}
 
-  readonly srcs$ = new Subject<SafeUrl[]>();
+  ngOnDestroy(): void {
+    this.srcs$.value.forEach((src) => URL.revokeObjectURL(src.url));
+    this.destroyed$.next(true);
+  }
 
   selectFile(event: Event) {
     const file: File = (event.target as EventTarget & { files: FileList })
       .files?.[0];
 
     if (file) {
-      this.onsetsService.splitFile(file).subscribe({
-        next: (urls) => {
-          // TODO: Revoke old urls
-          this.srcs$.next(
-            urls.map((url) => this.sanitizer.bypassSecurityTrustUrl(url))
-          );
-        },
-      }); // TODO: Until destroyed
+      this.onsetsService
+        .splitFile(file)
+        .pipe(takeUntil(this.destroyed$))
+        .subscribe({
+          next: (blobs) => {
+            const srcs = blobs.map((blob, i) => {
+              const url = URL.createObjectURL(blob);
+              const src = {
+                name: `${((i + 1) / 1000).toFixed(3).substring(2)}.mp3`,
+                blob: blob,
+                url: url,
+                safeUrl: this.sanitizer.bypassSecurityTrustUrl(url),
+              };
+              return src;
+            });
+            this.srcs$.next(srcs);
+            let zip = new JSZip();
+            srcs.forEach((src, i) => {
+              zip.folder(`${file?.name}.zip`)?.file(src.name, src.blob, {
+                compression: 'STORE',
+              });
+            });
+            zip
+              .generateAsync({ type: 'blob', compression: 'STORE' })
+              .then((zipBlob) => {
+                const zipURL = URL.createObjectURL(zipBlob);
+                const link = document.createElement('a');
+                link.setAttribute('href', zipURL);
+                link.setAttribute('download', `${file.name}.zip`);
+                link.click();
+                URL.revokeObjectURL(zipURL);
+              });
+          },
+        });
     }
   }
 }
